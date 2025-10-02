9837589be75fd5c6574b2efd8a7fd907050aded7
Nathan Barnes <nathanpaulbarnes@gmail.com>
2025-10-02 17:17:44 -0500

    Add Steam.

--- configuration.nix
+++ configuration.nix
@@ -99,6 +99,13 @@
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   # programs.mtr.enable = true;
   # programs.gnupg.agent = {
   #   enable = true;
