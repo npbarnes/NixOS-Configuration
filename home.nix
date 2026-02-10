7179444ecbd1707610d1ba1f44e2afce5ab52564
Nathan Barnes <nathanpaulbarnes@gmail.com>
2026-02-10 16:46:40 -0600

    Add spell check dictionaries

--- home.nix
+++ home.nix
@@ -13,6 +13,7 @@
     ./modules/home-manager/firefox.nix
     ./modules/home-manager/git.nix
     ./modules/home-manager/direnv.nix
+    ./modules/home-manager/spell-check.nix
   ];
 
   home.packages = [
---
+++ modules/home-manager/spell-check.nix
@@ -0,0 +1,11 @@
+# Adds spell checking and dictionaries that are used by many text editors.
+{pkgs, ...} : {
+  home.packages = [
+    pkgs.hunspell
+    pkgs.hunspellDicts.en_US-large
+    pkgs.aspell
+    pkgs.aspellDicts.en
+    pkgs.aspellDicts.en-science
+    pkgs.aspellDicts.en-computers
+  ];
+}
