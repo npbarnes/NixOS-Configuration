435a2e68124dd00c661b87077fc72273c53c8451
Nathan Barnes <nathanpaulbarnes@gmail.com>
2026-02-12 16:01:09 -0600

    Add some printer drivers and enable autodiscovery of network printers

--- modules/nixos/printing.nix
+++ modules/nixos/printing.nix
@@ -1,3 +1,17 @@
-{
-  services.printing.enable = true;
+{pkgs, ...} : {
+  services = {
+    printing = {
+      enable = true;
+      drivers = [
+        pkgs.gutenprint
+        pkgs.gutenprintBin
+        pkgs.hplip
+      ];
+    };
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+  };
 }
