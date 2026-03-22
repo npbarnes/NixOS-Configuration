7f4cf2b78a384ba60e1f064e2440d933706ec89a
Nathan Barnes <nathanpaulbarnes@gmail.com>
2026-03-21 22:02:30 -0500

    Bind qbittorrent to ivpn.

--- modules/nixos/torrents.nix
+++ modules/nixos/torrents.nix
@@ -1,5 +1,9 @@
 {pkgs, ...} : {
   services.qbittorrent = {
     enable = true;
+    serverConfig.BitTorrent.Session = {
+      Interface = "wgivpn";
+      InterfaceName = "wqivpn";
+    };
   };
 }
