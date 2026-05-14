ad35ac37e9d2469e2c047663bd84c277ede34458
Nathan Barnes <nathanpaulbarnes@gmail.com>
2026-05-13 23:26:55 -0500

    Adds Proton GE

--- modules/nixos/steam.nix
+++ modules/nixos/steam.nix
@@ -4,6 +4,10 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+
+    extraCompatPackages = [
+      pkgs.proton-ge-bin
+    ];
   };
 
   environment.systemPackages = [
