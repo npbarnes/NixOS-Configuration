172532f53eb983ddbcb808ce48c100d040850625
Nathan Barnes <nathanpaulbarnes@gmail.com>
2026-08-08 16:20:03 -0500

    Change lid close to suspend then hibernate

--- hosts/latitude/modules/nixos/power-states.nix
+++ hosts/latitude/modules/nixos/power-states.nix
@@ -1,20 +1,22 @@
 {
-  # Use systemd to suspend then hibernate when the power button is pressed
   services.logind.settings.Login = {
     HandlePowerKey = "suspend-then-hibernate";
+    HandleLidSwitch = "suspend-then-hibernate";
+    HandleLidSwitchDocked = "ignore";
     HibernateDelaySec = "2h";
   };
 
-  # Block GNOME from taking control of the power button
+  # Block GNOME from overriding systemd power state management
   security.polkit.extraConfig = ''
     polkit.addRule(function(action, subject) {
-      if (action.id == "org.freedesktop.login1.inhibit-handle-power-key") {
+      if (action.id == "org.freedesktop.login1.inhibit-handle-power-key" ||
+          action.id == "org.freedesktop.login1.inhibit-handle-lid-switch") {
         return polkit.Result.NO;
       }
     });
   '';
 
-  # Tell GNOME's internal settings to ignore the button
+  # Tell GNOME's internal settings to ignore the power button
   services.desktopManager.gnome.extraGSettingsOverrides = ''
     [org.gnome.settings-daemon.plugins.power]
     power-button-action='nothing'
