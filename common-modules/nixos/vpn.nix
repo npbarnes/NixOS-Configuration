dbf1c84bc151d92d0f80b3aa92d8206cd2747fcc
Nathan Barnes <nathanpaulbarnes@gmail.com>
2026-08-08 16:36:47 -0500

    Use systemd to autostart vpn

--- common-modules/nixos/vpn.nix
+++ common-modules/nixos/vpn.nix
@@ -1,6 +1,24 @@
 {pkgs, ...} : {
-  # Activate IVPN service and install desktop UI.
   services.ivpn.enable = true;
-
   environment.systemPackages = [ pkgs.ivpn-ui ];
+
+  systemd.services.ivpn-autoconnect = {
+    description = "IVPN autoconnect service";
+    requires = [ "network-online.target" "ivpn-service.service" ];
+    after = [ "network-online.target" "ivpn-service.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    path = [ pkgs.ivpn ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStartPre = "${pkgs.coreutils}/bin/sleep 2";
+      ExecStop = "${pkgs.ivpn}/bin/ivpn disconnect";
+      RemainAfterExit = true;
+    };
+
+    script = ''
+      ivpn connect -any
+    '';
+  };
 }
