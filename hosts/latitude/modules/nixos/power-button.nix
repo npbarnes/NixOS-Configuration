e4fdd0eb69e56c94cee504bde6b9862ee76c6581
Nathan Barnes <nathanpaulbarnes@gmail.com>
2026-08-08 15:41:03 -0500

    Prevent GNOME from overriding the systemd power button setting

--- hosts/latitude/modules/nixos/power-button.nix
+++ hosts/latitude/modules/nixos/power-button.nix
@@ -1,7 +1,22 @@
 {
+  # Use systemd to suspend then hibernate when the power button is pressed
   services.logind.settings.Login = {
-    # Suspend then hibernate when the power button is pressed
     HandlePowerKey = "suspend-then-hibernate";
     HibernateDelaySec = "2h";
   };
+
+  # Block GNOME from taking control of the power button
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if (action.id == "org.freedesktop.login1.inhibit-handle-power-key") {
+        return polkit.Result.NO;
+      }
+    });
+  '';
+
+  # Tell GNOME's internal settings to ignore the button
+  services.desktopManager.gnome.extraGSettingsOverrides = ''
+    [org.gnome.settings-daemon.plugins.power]
+    power-button-action='nothing'
+  '';
 }
