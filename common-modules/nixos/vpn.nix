a64fc523355939026d59768d76fba9eb6b7667a7
Nathan Barnes <nathanpaulbarnes@gmail.com>
2026-08-09 01:05:28 -0500

    Remove ivpn-autoconnect service. Prefer GUI autoconnect.

--- common-modules/nixos/vpn.nix
+++ common-modules/nixos/vpn.nix
@@ -1,24 +1,4 @@
 {pkgs, ...} : {
   services.ivpn.enable = true;
   environment.systemPackages = [ pkgs.ivpn-ui ];
-
-  systemd.services.ivpn-autoconnect = {
-    description = "IVPN autoconnect service";
-    requires = [ "network-online.target" "ivpn-service.service" ];
-    after = [ "network-online.target" "ivpn-service.service" ];
-    wantedBy = [ "multi-user.target" ];
-
-    path = [ pkgs.ivpn ];
-
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStartPre = "${pkgs.coreutils}/bin/sleep 2";
-      ExecStop = "${pkgs.ivpn}/bin/ivpn disconnect";
-      RemainAfterExit = true;
-    };
-
-    script = ''
-      ivpn connect -any -city chicago
-    '';
-  };
 }
