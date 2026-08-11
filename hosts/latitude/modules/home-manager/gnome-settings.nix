a407da04d77580658ed3d5e5099d6336141aefec
Nathan Barnes <nathanpaulbarnes@gmail.com>
2026-08-11 13:36:48 -0500

    Add more settings

--- hosts/latitude/modules/home-manager/gnome-settings.nix
+++ hosts/latitude/modules/home-manager/gnome-settings.nix
@@ -4,5 +4,22 @@
       audible-bell = false;
       ignore-scrollback-limit = true;
     };
+    "/org/gnome/settings-daemon/plugins/power" = {
+      power-button-action = "hibernate";
+    };
+    "/org/gnome/desktop/interface" = {
+      show-battery-percentage = true;
+    };
+    "/org/gnome/desktop/screensaver" = {
+      lock-delay = lib.hm.gvariant.mkUint32 300; # seconds
+    };
+    "/org/gnome/desktop/notifications" = {
+      show-in-lock-screen = false;
+    };
+    "/org/gnome/desktop/privacy" = {
+      recent-files-max-age = 7;
+      remove-old-trash-files = true;
+      remove-old-temp-files = true;
+    };
   };
 }
