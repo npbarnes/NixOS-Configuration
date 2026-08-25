22dac0d6325cecfeed61f89e27fc13385c67065b
Nathan Barnes <nathanpaulbarnes@gmail.com>
2026-08-25 13:58:27 -0500

    Add Steam Deck specific changes
    Note that Jovian only officially supports NixOS-unstable. I'm
    running it on stable because it sounds like most of the probelms come
    from gamescope and SteamUI, which I'm not using.

--- flake.nix
+++ flake.nix
@@ -9,6 +9,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    jovian = {
+      url = "github:Jovian-Experiments/Jovian-NixOS";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
--- hosts/deck/configuration.nix
+++ hosts/deck/configuration.nix
@@ -1,4 +1,15 @@
-{
+{ inputs, ... }: {
+  imports = [
+    inputs.jovian.nixosModules.default
+  ];
+
+  # Enable Steam Deck specific patches
+  jovian.devices.steamdeck.enable = true;
+
+  # Do NOT enable the Steam Deck console-like experience
+  jovian.steam.enable = false;
+  jovian.steam.autoStart = false;
+
   system.stateVersion = "25.05";
   home-manager.users.npbarnes.home.stateVersion = "24.11";
 }
