29d6d3d227c8392c718b87277062b4a29f9671a4
Nathan Barnes <nathanpaulbarnes@gmail.com>
2026-08-28 14:21:24 -0500

    Autologin on latitude with full disk encryption

--- hosts/latitude/configuration.nix
+++ hosts/latitude/configuration.nix
@@ -1,4 +1,10 @@
 {
+  # Full disk encryption makes autoLogin secure
+  services.displayManager.autoLogin = {
+    enable = true;
+    user = "npbarnes";
+  };
+
   system.stateVersion = "26.05";
   home-manager.users.npbarnes.home.stateVersion = "26.05";
 }
