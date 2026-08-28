4e6f3a4fc74b055db9312f7e415007f5c46b1133
Nathan Barnes <nathanpaulbarnes@gmail.com>
2026-08-27 19:38:22 -0500

    Enable most Jovian features

--- hosts/deck/modules/nixos/steamdeck.nix
+++ hosts/deck/modules/nixos/steamdeck.nix
@@ -9,9 +9,17 @@
   ];
 
   # Enable Steam Deck specific patches
-  jovian.devices.steamdeck.enable = true;
+  jovian.devices.steamdeck = {
+    enable = true;
+    autoUpdate = true; # Firmware updates
+  };
 
-  # Do NOT enable the Steam Deck console-like experience
-  jovian.steam.enable = false;
+  # Enable SteamOS specific settings
+  jovian.steamos.useSteamOSConfig = true;
+
+  # Enable the Steam Deck console-like experience
+  jovian.steam.enable = true;
+
+  # Do NOT boot directly into Steam (use `start-gamescope-session` command instead)
   jovian.steam.autoStart = false;
 }
