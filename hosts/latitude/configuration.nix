7b82079249fcd20be2e9d08ff0a3380ef204729f
Nathan Barnes <nathanpaulbarnes@gmail.com>
2026-09-04 17:14:14 -0500

    Disable autoLogin since it interferes with keyring unlocking

--- hosts/latitude/configuration.nix
+++ hosts/latitude/configuration.nix
@@ -1,10 +1,4 @@
 {
-  # Full disk encryption makes autoLogin secure
-  services.displayManager.autoLogin = {
-    enable = true;
-    user = "npbarnes";
-  };
-
   system.stateVersion = "26.05";
   home-manager.users.npbarnes.home.stateVersion = "26.05";
 }
