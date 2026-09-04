e4172cf9df865cb15ec4139bbda3b1489c26e406
Nathan Barnes <nathanpaulbarnes@gmail.com>
2026-09-04 16:52:43 -0500

    Add Bitwarden extension to Firefox

--- common/modules/home-manager/bitwarden.nix
+++ common/modules/home-manager/bitwarden.nix
@@ -9,4 +9,14 @@
   home.sessionVariables = {
     SSH_AUTH_SOCK = "$HOME/.bitwarden-ssh-agent.sock";
   };
+
+  # Add Bitwarden extension to Firefox
+  programs.firefox.policies.ExtensionSettings = {
+    "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+      install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+      installation_mode = "normal_installed";
+      default_area = "navbar";
+      private_browsing = true;
+    };
+  };
 }
