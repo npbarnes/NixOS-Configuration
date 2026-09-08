6faffced7a24038b4e6c2166663ec065fe4d3038
Nathan Barnes <nathanpaulbarnes@gmail.com>
2026-09-08 13:13:18 -0500

    Change Gnome terminal to dark theme.

--- hosts/latitude/modules/home-manager/terminal.nix
+++ hosts/latitude/modules/home-manager/terminal.nix
@@ -5,9 +5,33 @@
       visibleName = "Default";
       default = true;
       customCommand = "fish";
-      allowBold = true;
       audibleBell = false;
       scrollbackLines = 1000000;
+      allowBold = true;
+      boldIsBright = true;
+      colors = {
+        # Gnome Dark theme with Gnome color palette
+        foregroundColor = "rgb(208,207,204)";
+        backgroundColor = "rgb(23,20,33)";
+        palette = [
+          "rgb(23,20,33)"
+          "rgb(192,28,40)"
+          "rgb(38,162,105)"
+          "rgb(162,115,76)"
+          "rgb(18,72,139)"
+          "rgb(163,71,186)"
+          "rgb(42,161,179)"
+          "rgb(208,207,204)"
+          "rgb(94,92,100)"
+          "rgb(246,97,81)"
+          "rgb(51,209,122)"
+          "rgb(233,173,12)"
+          "rgb(42,123,222)"
+          "rgb(192,97,203)"
+          "rgb(51,199,222)"
+          "rgb(255,255,255)"
+        ];
+      };
     };
   };
 }
