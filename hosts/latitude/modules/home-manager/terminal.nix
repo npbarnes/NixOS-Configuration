e178844aeb9d5a17a204ecf470a11eb24ef1547c
Nathan Barnes <nathanpaulbarnes@gmail.com>
2026-09-08 00:48:54 -0500

    Switch from Ptyxis to Gnome Terminal

--- hosts/latitude/modules/home-manager/terminal.nix
+++ hosts/latitude/modules/home-manager/terminal.nix
@@ -1,3 +1,13 @@
 {
-  programs.ptyxis.enable = true;
+  programs.gnome-terminal = {
+    enable = true;
+    profile."27a967ce-38c1-40c3-a6ca-37038ca7be40" = {
+      visibleName = "Default";
+      default = true;
+      customCommand = "fish";
+      allowBold = true;
+      audibleBell = false;
+      scrollbackLines = 1000000;
+    };
+  };
 }
