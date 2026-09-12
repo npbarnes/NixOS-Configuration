3ddcf062c9bffa335bfff071308cc451b351b7a1
Nathan Barnes <nathanpaulbarnes@gmail.com>
2026-09-12 11:42:15 -0500

    Point Gnome Terminal custom command into the Nix Store

--- hosts/latitude/modules/home-manager/terminal.nix
+++ hosts/latitude/modules/home-manager/terminal.nix
@@ -1,10 +1,10 @@
-{
+{ pkgs, ... }: {
   programs.gnome-terminal = {
     enable = true;
     profile."27a967ce-38c1-40c3-a6ca-37038ca7be40" = {
       visibleName = "Default";
       default = true;
-      customCommand = "fish";
+      customCommand = "${pkgs.fish}/bin/fish";
       audibleBell = false;
       scrollbackLines = 1000000;
       allowBold = true;
