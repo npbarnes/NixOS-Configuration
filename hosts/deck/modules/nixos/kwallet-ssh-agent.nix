debaf34f9cf72321ec92add1a8917e5c3985806f
Nathan Barnes <nathanpaulbarnes@gmail.com>
2026-09-11 23:22:48 -0500

    Enable ask password

--- hosts/deck/modules/nixos/kwallet-ssh-agent.nix
+++ hosts/deck/modules/nixos/kwallet-ssh-agent.nix
@@ -3,6 +3,7 @@
 
   security.pam.services.sddm.enableKwallet = true;
 
+  programs.ssh.enableAskPassword = true;
   programs.ssh.askPassword = "${pkgs.kdePackages.ksshaskpass}/bin/ksshaskpass";
   environment.variables.SSH_ASKPASS_REQUIRE = "prefer";
 }
