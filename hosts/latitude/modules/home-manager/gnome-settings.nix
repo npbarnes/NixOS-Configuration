edd8a9b87b8beec7a89286725e29580aab9bc299
Nathan Barnes <nathanpaulbarnes@gmail.com>
2026-09-12 19:10:54 -0500

    Switch pinned terminal to Alacritty

--- hosts/latitude/modules/home-manager/gnome-settings.nix
+++ hosts/latitude/modules/home-manager/gnome-settings.nix
@@ -4,7 +4,7 @@
       favorite-apps = [
         "org.gnome.Nautilus.desktop"
         "firefox.desktop"
-        "org.gnome.Terminal.desktop"
+        "Alacritty.desktop"
         "steam.desktop"
       ];
     };
